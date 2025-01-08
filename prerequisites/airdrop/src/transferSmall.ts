d5683c756bf10b3c16edd51afdf4440b1c972012
davidjian0905 <davidjian0905@gmail.com>
2025-01-08 14:14:50 +0800

    move scripts into src folder

--- prerequisites/airdrop/src/transferSmall.ts
+++ prerequisites/airdrop/src/transferSmall.ts
@@ -7,7 +7,7 @@ import {
   sendAndConfirmTransaction,
   PublicKey,
 } from "@solana/web3.js";
-import wallet from "./dev-wallet.json";
+import wallet from "./../dev-wallet.json";
 
 const from = Keypair.fromSecretKey(new Uint8Array(wallet));
 
@@ -16,14 +16,11 @@ const connection = new Connection("https://api.devnet.solana.com");
 
 (async () => {
   try {
-    // Get balance of dev wallet
-    const balance = await connection.getBalance(from.publicKey);
-
     const transaction = new Transaction().add(
       SystemProgram.transfer({
         fromPubkey: from.publicKey,
         toPubkey: to,
-        lamports: balance,
+        lamports: LAMPORTS_PER_SOL / 10,
       })
     );
     transaction.recentBlockhash = (
@@ -31,25 +28,7 @@ const connection = new Connection("https://api.devnet.solana.com");
     ).blockhash;
 
     transaction.feePayer = from.publicKey;
-    // Calculate exact fee rate to transfer entire SOL amount out of account minus fees
-    const fee =
-      (
-        await connection.getFeeForMessage(
-          transaction.compileMessage(),
-          "confirmed"
-        )
-      ).value || 0;
-    // Remove our transfer instruction to replace it
-    transaction.instructions.pop();
-    // Now add the instruction back with correct amount oflamports
 
-    transaction.add(
-      SystemProgram.transfer({
-        fromPubkey: from.publicKey,
-        toPubkey: to,
-        lamports: balance - fee,
-      })
-    );
     // Sign transaction, broadcast, and confirm
     const signature = await sendAndConfirmTransaction(connection, transaction, [
       from,
