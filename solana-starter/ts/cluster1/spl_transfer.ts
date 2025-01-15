c5f7a3b0d506417cc5a00ee8f1cc00bc78240ada
davidjian0905 <davidjian0905@gmail.com>
2025-01-15 23:25:54 +0800

    worked on spl_metadata and spl_transfer

--- solana-starter/ts/cluster1/spl_transfer.ts
+++ solana-starter/ts/cluster1/spl_transfer.ts
@@ -1,5 +1,11 @@
-import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
-import wallet from "../wba-wallet.json"
+import {
+  Commitment,
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+} from "@solana/web3.js";
+import wallet from "../wba-wallet.json";
 import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
 
 // We're going to import our keypair from the wallet file
@@ -10,19 +16,43 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("E7K69EEoKb9d3SXGqQN2tViX1Nus48zrDT9wNyRDDJqZ");
 
 // Recipient address
-const to = new PublicKey("<receiver address>");
+const to = new PublicKey("HPVUnjfkQWet7uMDDAtUNbkGZVSjb54GVMjpJwHYZoHT");
 
 (async () => {
-    try {
-        // Get the token account of the fromWallet address, and if it does not exist, create it
+  try {
+    // Get the token account of the fromWallet address, and if it does not exist, create it
+    const fromAtaAddress = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      keypair.publicKey,
+      false,
+      commitment
+    );
+    // Get the token account of the toWallet address, and if it does not exist, create it
+    const toAtaAddress = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      to,
+      false,
+      commitment
+    );
 
-        // Get the token account of the toWallet address, and if it does not exist, create it
-
-        // Transfer the new token to the "toTokenAccount" we just created
-    } catch(e) {
-        console.error(`Oops, something went wrong: ${e}`)
-    }
+    // Transfer the new token to the "toTokenAccount" we just created
+    const tx = await transfer(
+      connection,
+      keypair,
+      fromAtaAddress.address,
+      toAtaAddress.address,
+      keypair.publicKey,
+      10000 //0.01 DAV
+    );
+    console.log(`Your txid: ${tx}`);
+  } catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
+  }
 })();
